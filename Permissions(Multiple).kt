705b8f61a6b726408c3602c35fc8c771e832d9f8
Abhisathvika(12215270) <118842724+Abhisathvika@users.noreply.github.com>
2024-10-28 13:49:22 +0530

    Create Permissions(Multiple).kt
    Added the code for allowing or denying multiple permissions

---
+++ Permissions(Multiple).kt
@@ -0,0 +1,41 @@
+package com.example.flowerbouquetapplication
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainPermission : AppCompatActivity() {
+    var c = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_permission)
+        var b1 = findViewById<Button>(R.id.btn)
+        b1.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),c)
+            }
+            else{
+                Toast.makeText(this,"Already granted",Toast.LENGTH_LONG).show()
+            }
+            }
+
+        }
+    override fun onRequestPermissionsResult (requestCode:Int, permissions: Array<out  String>, grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==c){
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+
+            }
+            else{
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
